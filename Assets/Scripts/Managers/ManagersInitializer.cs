a75c29b3b8b7b37091998e6d3ee763bf65cbd360
RusyaZamorin <59511793+RusyaZamorin@users.noreply.github.com>
2021-02-09 15:52:28 +0200

    [+] add manager initializer and container

---
+++ Assets/Scripts/Managers/ManagersInitializer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers
+{
+    public class ManagerInitializer : MonoBehaviour
+    {
+        private void Init()
+        {
+
+        }
+
+        private void Awake()
+        {
+            Init();
+        }
+    }
+}
+
+
